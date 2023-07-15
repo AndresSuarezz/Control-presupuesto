ab9dd2c48b52f391872efda5168df97981f9a733
AndresSuarezz <anfesuguito22@gmail.com>
2023-07-15 18:08:46 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Add from "./components/Add";
+
+function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  return (
+    <div>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      {isValidPresupuesto && (
+        <Add />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Add.jsx
@@ -0,0 +1,11 @@
+import IconoNuevoGasto from "../img/nuevo-gasto.svg";
+
+const Add = () => {
+  return (
+    <div className="nuevo-gasto">
+      <img src={IconoNuevoGasto} alt="nuevo gasto" />
+    </div>
+  );
+};
+
+export default Add;
